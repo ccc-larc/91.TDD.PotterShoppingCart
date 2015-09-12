e4a22171c65d04ecc9b254b2de7018459a8f060c
ccc <ccc@80-cloud.com>
2015-09-12 22:43:58 +0800

    Scenario: 第一集買了一本，其他都沒買，價格應為 100 * 1 = 100 元

--- potter_shopping_cart.py
+++ potter_shopping_cart.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+POTTER_VOL_1 = 1
+
+BOOK_PRICE = 100
+
+
+class Cart(object):
+
+    def __init__(self):
+        self._books = {}
+
+    def add_book(self, book_id):
+        self._books[book_id] = self._books.get(book_id, 0) + 1
+
+    def get_price(self):
+        return BOOK_PRICE * sum(self._books.itervalues(), 0)
--- tests/test_potter_shopping_cart.py
+++ tests/test_potter_shopping_cart.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 import unittest
+from potter_shopping_cart import Cart, POTTER_VOL_1
 
 
 class PotterShoppingCartTest(unittest.TestCase):
-    pass
+
+    def test_v1x1_should_price_100(self):
+        """
+        Scenario: 第一集買了一本，其他都沒買，價格應為 100 * 1 = 100 元
+            Given 第一集買了 1 本
+            And 第二集買了 0 本
+            And 第三集買了 0 本
+            And 第四集買了 0 本
+            And 第五集買了 0 本
+            When 結帳
+            Then 價格應為 100 元
+        """
+        # arrange
+        cart = Cart()
+
+        # act
+        cart.add_book(POTTER_VOL_1)
+
+        # assert
+        expected = 100
+        self.assertEqual(cart.get_price(), expected)
 
 
 if __name__ == '__main__':

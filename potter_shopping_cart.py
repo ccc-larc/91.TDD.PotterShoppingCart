1f9eda3bab0f9c888bd920e725f7f18164a3342d
ccc <ccc@80-cloud.com>
2015-09-12 23:15:18 +0800

    重構 : 加註解、extract _get_bookset_discount()

--- potter_shopping_cart.py
+++ potter_shopping_cart.py
@@ -19,14 +19,22 @@ class Cart(object):
 
     def get_price(self):
         book_count = sum(self._books.itervalues(), 0)
-        if book_count == 2:
-            discount = 0.95
-        elif book_count == 3:
-            discount = 0.9
-        elif book_count == 4:
-            discount = 0.8
-        elif book_count == 5:
-            discount = 0.75
-        else:
-            discount = 1.0
+        discount = _get_bookset_discount(set(self._books))
         return BOOK_PRICE * book_count * discount
+
+
+def _get_bookset_discount(bookset):
+    assert isinstance(bookset, set)
+
+    book_count = len(bookset)
+
+    if book_count == 2:  # 兩本套書
+        return 0.95
+    elif book_count == 3:  # 三本套書
+        return 0.9
+    elif book_count == 4:  # 四本套書
+        return 0.8
+    elif book_count == 5:  # 五本套書
+        return 0.75
+    else:  # 非套書
+        return 1.0
